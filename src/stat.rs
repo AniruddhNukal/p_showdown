a77e442b24ded369af94afa5c17f92ed17e42491
Aniruddh Nukal <avnukal@gmail.com>
2025-10-29 21:04:17 -0400

    Implement StatError

--- src/stat.rs
+++ src/stat.rs
@@ -1,13 +1,26 @@
 use std::cmp::Eq;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum StatError {
+    #[error("value of {value} cannot be stored in stat with bound of {bound}")]
+    OutOfBounds { value: u16, bound: u16 },
+}
 
 macro_rules! define_stat_newtype {
-    ($struct_name:ident, $mask:expr) => {
+    ($struct_name:ident, $upper_bound:expr) => {
         #[derive(Debug, Eq, PartialEq, Default, Clone, Copy)]
         pub struct $struct_name(u16);
 
         impl Stat for $struct_name {
-            fn new(i: u16) -> Self {
-                Self($mask & i)
+            fn new(i: u16) -> Result<Self, StatError> {
+                match i <= $upper_bound {
+                    true => Ok(Self(i)),
+                    false => Err(StatError::OutOfBounds {
+                        value: i,
+                        bound: $upper_bound,
+                    }),
+                }
             }
 
             fn get(&self) -> u16 {
@@ -17,7 +30,7 @@ macro_rules! define_stat_newtype {
     };
 
     ($struct_name:ident) => {
-        define_stat_newtype!($struct_name, 0xFFFF);
+        define_stat_newtype!($struct_name, u16::MAX);
     };
 }
 
@@ -77,15 +90,29 @@ impl<T: Stat> StatVec<T> {
         }
     }
 
-    pub fn from_int(hp: u16, atk: u16, def: u16, spa: u16, spd: u16, spe: u16) -> Self {
-        StatVec::<T> {
-            hp: T::new(hp),
-            atk: T::new(atk),
-            def: T::new(def),
-            spa: T::new(spa),
-            spd: T::new(spd),
-            spe: T::new(spe),
-        }
+    pub fn from_int(
+        hp: u16,
+        atk: u16,
+        def: u16,
+        spa: u16,
+        spd: u16,
+        spe: u16,
+    ) -> Result<Self, StatError> {
+        let hp = T::new(hp)?;
+        let atk = T::new(atk)?;
+        let def = T::new(def)?;
+        let spa = T::new(spa)?;
+        let spd = T::new(spd)?;
+        let spe = T::new(spe)?;
+
+        Ok(StatVec::<T> {
+            hp,
+            atk,
+            def,
+            spa,
+            spd,
+            spe,
+        })
     }
 }
 
@@ -107,7 +134,9 @@ impl StageVec {
 }
 
 pub trait Stat {
-    fn new(i: u16) -> Self;
+    fn new(i: u16) -> Result<Self, StatError>
+    where
+        Self: Sized;
     fn get(&self) -> u16;
 }
 
@@ -128,22 +157,6 @@ impl_stage_newtype!(AccEvaStage, 3.);
 #[cfg(test)]
 mod tests {
     use crate::stat::{EV, IV, Stage, Stat, StatStage};
-
-    #[test]
-    fn mask_test_ev() {
-        let proper = EV::new(2);
-        let improper = EV::new(258);
-
-        assert_eq!(proper, improper)
-    }
-
-    #[test]
-    fn mask_test_iv() {
-        let proper = IV::new(31);
-        let improper = IV::new(63);
-
-        assert_eq!(proper, improper)
-    }
 
     #[test]
     fn upper_bound_test_stage() {
